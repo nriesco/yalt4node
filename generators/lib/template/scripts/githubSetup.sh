e6e775db5f3c79593f4c69974b92041c581b524f
Nico R <riesco@gmail.com>
2022-01-24 11:54:14 -0300

    fix

--- generators/lib/template/scripts/githubSetup.sh
+++ generators/lib/template/scripts/githubSetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting ..."
-curl -u '[[YOUR_GITHUB_USERNAME_HERE]' https://api.github.com/user/repos -d '{"name":"lib-template-temp-name-must-replace"}'
+curl -u '[[YOUR_GITHUB_USERNAME_HERE]]' https://api.github.com/user/repos -d '{"name":"lib-template-temp-name-must-replace"}'
 git init
 git add .
 git commit -m "First commit"
